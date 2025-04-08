252d1cd34bb0f3d65028aaaa2617296078afe644
TeukuRyanIskandar <teukurian090598@gmail.com>
2025-04-09 01:26:28 +0800

    add default button to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,15 @@
-import type { StorybookConfig } from '@storybook/vue3-vite';
-
-const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
-  }
-};
-export default config;
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/components/**/*.stories.js'],
+   staticDirs: ['../public'],
+   addons: [
+     '@storybook/addon-links',
+     '@storybook/addon-essentials',
+     '@storybook/addon-interactions',
+   ],
+   framework: {
+     name: '@storybook/vue3-vite',
+     options: {},
+   },
+ };
+ export default config;
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,56 +1,26 @@
-import { fn } from '@storybook/test';
-import type { Meta, StoryObj } from '@storybook/vue3';
+import type { Meta, StoryFn } from '@storybook/vue3'
+import { Button } from '@/components/ui/button/index.ts'
 
-import Button from './Button.vue';
-
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta = {
-  title: 'Example/Button',
+export default {
+  title: 'shadcn/Button',
   component: Button,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
   argTypes: {
-    size: { control: 'select', options: ['small', 'medium', 'large'] },
-    backgroundColor: { control: 'color' },
+    variant: {
+      control: 'select',
+      options: ['default', 'destructive', 'outline', 'secondary', 'ghost', 'link'],
+    },
   },
-  args: {
-    primary: false,
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    onClick: fn(),
-  },
-} satisfies Meta<typeof Button>;
+} as Meta<typeof Button>
 
-export default meta;
-type Story = StoryObj<typeof meta>;
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/api/csf
- * to learn how to use render functions.
- */
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
+const Template: StoryFn<typeof Button> = (args) => ({
+  components: { Button },
+  setup() {
+    return { args }
   },
-};
+  template: `<Button v-bind="args">Click Me</Button>`,
+})
 
-export const Secondary: Story = {
-  args: {
-    primary: false,
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    label: 'Button',
-    size: 'large',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    label: 'Button',
-    size: 'small',
-  },
-};
+export const Default = Template.bind({})
+Default.args = {
+  variant: 'default',
+}
