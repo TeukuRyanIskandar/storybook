0436f391672fda45788da11d7603fccdba666e3b
TeukuRyanIskandar <teukurian090598@gmail.com>
2025-04-08 23:54:50 +0800

    install shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import tailwindcss from "tailwindcss"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
